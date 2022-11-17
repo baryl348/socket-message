a17f2115bd219cb8eb7782ad25fdcbe81b902893
Акинин А <akinin@tprs.ru>
2022-11-17 14:31:24 +0300

    new approach

--- index.js
+++ index.js
@@ -1,52 +1,54 @@
-const ws = require('ws');
+// const ws = require('websocket').server;
+const http = require('http');
+const https = require('https');
+const eventEmitter = require('events').EventEmitter;
+const url = require('url');
+const crypto = require('crypto');
 
-class Server {
-    ws; //websocket connect
-    port; // port for connect
-    constructor(
-        port,
-        options
-    ) {
-        this.#setPortConnection(port).then(
-            this.ws = new ws.Server({
-                port: this.port,
-            }, console.log(`[WS]: server stated on port ${this.port}`))
-        )  
-    }
+const PORT = 8081;
 
-    async #setPortConnection (port) {
-        if (!port) {
-            this.port = 5100;
-            return;
+const protocolSeparators = [
+    '(', ')', '<', '>', '@',
+    ',', ';', ':', '\\', '\"',
+    '/', '[', ']', '?', '=',
+    '{', '}', ' ', String.fromCharCode(9)
+];
+
+const excludedTlsOptions = ['hostname', 'port', 'method', 'path', 'headers'];
+
+const server = http.createServer((request, response) => {
+    response.writeHead(404);
+    response.end();
+});
+
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${8081}`)
+})
+
+
+class WebSocketClient {
+    constructor(options) {
+        eventEmitter.call(this);
+        this.options = {
+            // максимальный размер 1 (frame)
+            maxReceivedFrameSize: 0x100000,
+
+            // максимальный размер сообщения 8 
+            maxReceivedMessageSize: 0x800000,
+
+            fragmentOutgoingMessages: true,
+
+            // фрагментируется если превышает объем 16кб
+
+            fragmentationThreshold: 0x4000,
+
+            // версия протокола сокета 
+
+            webSocketVersion: 13,
+
+            // закрыть порт по истечению времени
+
+            closedTimeout: 5000
         }
-        this.port = port;
-    }
-
-    startConnection() {
-        this.ws.on('connection', (ws) => {
-            ws.id = new Date.now(); // id for websocket connection users
-            ws.on('message', (ms) => {
-                ms = JSON.parse(ms);
-                switch (ms.event) {
-                    case 'message':
-                        this.#broadcastMessage(ms, id);
-                        break;
-                    case 'connection':
-                        this.#broadcastMessage(ms, id);
-                        break;
-                }
-            })
-        })
-    }
-
-    #broadcastMessage (ms, id) {
-        // id for secure room
-        this.ws.clients.forEach((client) => {
-            if (client.id === id) {
-                client.send(JSON.stringify(ms));
-            }
-        })
     }
 }
-
-export default Server;
